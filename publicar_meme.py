2a23a9af354ddb0f2e78122ded9c1a1c24152468
Aleix Segú <aleix.segu@estudiants.urv.cat>
2026-07-21 18:44:34 +0200

    feat: add "elpajaroopina" to excluded TikTok accounts for improved meme curation

--- publicar_meme.py
+++ publicar_meme.py
@@ -95,7 +95,6 @@ BUSQUEDAS_OBJETIVO = [
     "meme",
     "memes",
     "shitpost",
-    "memes mundial",
     "memes xokas",
     "memes chiringuito",
     "memes futbol",
@@ -113,7 +112,7 @@ CUENTAS_EXCLUIDAS = {"failets", "lobostroy", "el.borov.memes", "vristok", "alway
                      "rmelendi", "spana.papi", "sol.m.t.c", "shortsenaguer", "siaeiros_cdl", "krsiveniz", "itziarcasanova", "ravebg_ai", "paulalamc",
                      "aitor.as", "genpo28", "deadpoolmadrid", "lizi_culer", "blaaugrana_", "7alvaricoke.2_", "danimoraales10", "bailafm", "aiaiai.ai",
                      "anonimo17213", "elrelatodeportivo", "zellendustreal", "psg", "mismemesymas", "bestiblaze_.oficial", "monicahumor86", "teamd8pro",
-                     "thegalaxy966", "rdefurbol", "samuelsupongo", "oopsitsmj", "gestnub", "xxbrexx1"}
+                     "thegalaxy966", "rdefurbol", "samuelsupongo", "oopsitsmj", "gestnub", "xxbrexx1", "elpajaroopina"}
 
 # Al menos uno de estos términos debe aparecer en la descripción del vídeo.
 # Garantiza que el contenido sea efectivamente un meme o humor, y no un vídeo
