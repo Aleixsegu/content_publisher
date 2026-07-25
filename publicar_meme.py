732cba3071a9e9b0b7ef794abdd97351b7a516a8
Aleix Segú <aleix.segu@estudiants.urv.cat>
2026-07-25 17:44:15 +0200

    feat: update target searches and excluded TikTok accounts for improved meme curation

--- publicar_meme.py
+++ publicar_meme.py
@@ -98,7 +98,7 @@ BUSQUEDAS_OBJETIVO = [
     "memes xokas",
     "memes chiringuito",
     "memes futbol",
-    "memes pereira",
+    "memes velada"
 ]
 
 # Región objetivo para filtrar resultados
@@ -112,7 +112,8 @@ CUENTAS_EXCLUIDAS = {"failets", "lobostroy", "el.borov.memes", "vristok", "alway
                      "rmelendi", "spana.papi", "sol.m.t.c", "shortsenaguer", "siaeiros_cdl", "krsiveniz", "itziarcasanova", "ravebg_ai", "paulalamc",
                      "aitor.as", "genpo28", "deadpoolmadrid", "lizi_culer", "blaaugrana_", "7alvaricoke.2_", "danimoraales10", "bailafm", "aiaiai.ai",
                      "anonimo17213", "elrelatodeportivo", "zellendustreal", "psg", "mismemesymas", "bestiblaze_.oficial", "monicahumor86", "teamd8pro",
-                     "thegalaxy966", "rdefurbol", "samuelsupongo", "oopsitsmj", "gestnub", "xxbrexx1", "elpajaroopina"}
+                     "thegalaxy966", "rdefurbol", "samuelsupongo", "oopsitsmj", "gestnub", "elpajaroopina", "zurdazoo", "noticierotv3", "klasick.project2",
+                     "infojobs", "24maiastlss", "aitanafulstream", }
 
 # Al menos uno de estos términos debe aparecer en la descripción del vídeo.
 # Garantiza que el contenido sea efectivamente un meme o humor, y no un vídeo
